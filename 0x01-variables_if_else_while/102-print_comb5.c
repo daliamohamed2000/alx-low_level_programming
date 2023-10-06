ae9cdcd67b8e2be7c43032cf4dcda2d36555c285
daliamohamed2000 <dm1956@fayoum.edu.eg>
2023-10-06 16:36:43 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int fd = 0, sd;
+
+	while (fd <= 99)
+	{
+		sd = fd;
+		while (sd <= 99)
+		{
+			if (sd != fd)
+			{
+				putchar((fd / 10) + 48);
+				putchar((fd % 10) + 48);
+				putchar(' ');
+				putchar((sd / 10) + 48);
+				putchar((sd % 10) + 48);
+
+				if (fd != 98 || sd != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			sd++;
+		}
+		fd++;
+	}
+	putchar('\n');
+return (0);
+}
