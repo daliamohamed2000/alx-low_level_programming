570b9bfbcb0cb954c45de8cd44ca90ebe6a53673
daliamohamed2000 <dm1956@fayoum.edu.eg>
2023-10-13 15:50:37 -0700

    0-main.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,14 +7,11 @@
 * @month: month in number format
 * @day: day of month
 * @year: year
+* Return: void
 */
 
 void print_remaining_days(int month, int day, int year)
 {
-	/**
-	 * leap year when it can be divided
-	 * by 100 and 400 or by 4 evently
-	*/
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 100 == 0))
 	{
 		if (month > 2 && day >= 60)
