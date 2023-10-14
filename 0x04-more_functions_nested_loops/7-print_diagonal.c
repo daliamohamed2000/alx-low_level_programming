26b816b22c2de8f6a364fdbca1f456b577169502
daliamohamed2000 <dm1956@fayoum.edu.eg>
2023-10-13 17:08:53 -0700

    int _isupper(int c);

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ *
+ * @n: number of times that character _
+ * should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int i, s;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (s = 1; s <= i; s++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
