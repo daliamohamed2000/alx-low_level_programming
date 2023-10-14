9d891625b2abf1b974c7970aa2c5fb83bb449818
daliamohamed2000 <dm1956@fayoum.edu.eg>
2023-10-13 17:01:36 -0700

    int _isupper(int c);

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,22 @@
 #include "main.h"
 
 /**
- * 
+ * print_line - draws a straight line in the terminal.
+ *
+ * @n: number of times that character _
+ * should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
