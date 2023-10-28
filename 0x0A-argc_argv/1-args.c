83f816b6474e20e05a8f23b4ca65227679888686
daliamohamed2000 <dm1956@fayoum.edu.eg>
2023-10-27 18:20:30 -0700

    def

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the minimum number of coins
+ * to make change for an amount of money.
+ * @argc: number of command line argument
+ * @argv: pointer to an array of command line argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
