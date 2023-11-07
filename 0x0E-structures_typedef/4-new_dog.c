c8ce3586da245eca707ced2a1ee51471d671f5f3
daliamohamed2000 <dm1956@fayoum.edu.eg>
2023-11-07 14:59:20 -0800

    ds

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,76 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * _strlen - a function that getsa length of a string
+ *
+ * @str: the string to get the length
+ *
+ * Return: length of @str
+ */
+
+int _strlen(const char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
+
+/**
+ * _strcopy - a function that returns @dest
+ * with a copy of a string from @src
+ *
+ * @src: string to copy
+ * @dest: copy string to here
+ *
+ * Return: @dest
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - a function that creates a new dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Return: struct pointer dog
+ * NULL if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == Null)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	d->name = _strcopy(d->name, name);
+	d->age = age;
+	d->owner = _strcopy(d->owner, owner);
+
+	return (dog);
+}
