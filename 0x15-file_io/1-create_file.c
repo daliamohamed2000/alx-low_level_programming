34e1dd6d12d1763a49ee22c3d8870073510934ed
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-03-12 20:53:56 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,8 +2,8 @@
 
 /**
  * create_file - create a file
- * @fn: filename
- * @tc: text content
+ * @filename: filename
+ * @text_content: text content
  *
  * Return: 1 if success, else -1
  */
@@ -13,21 +13,21 @@ int create_file(const char *filename, char *text_content)
 	int nl;
 	int rwr;
 
-	if (!fn)
+	if (!filename)
 		return (-1);
 
-	fd = open(fn, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (!tc)
-		tc = "";
+	if (!text_content)
+		text_content = "";
 
-	for (nl = 0; tc[nl]; nl++)
+	for (nl = 0; text_content[nl]; nl++)
 		;
 
-	rwr = write(fd, tc, nl);
+	rwr = write(fd, text_content, nl);
 
 	if (rwr == -1)
 		return (-1);
