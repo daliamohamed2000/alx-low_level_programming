21b9ac89c6032d5b3207bd70c00105b845632320
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-04-24 19:07:29 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -98,6 +98,12 @@ void print_all(const char * const format, ...)
 	while (format && format[i])
 	{
 		j = 0;
+		/**
+		 * 4 equals to the number of funcs present
+		 * so if j is less than four and our current
+		 * format is not equal to format in funcs
+		 * then j becomes j + 1
+		 */
 		while (j < 4 && (format[i] != *(funcs[j].symbol)))
 			j++;
 		if (j < 4)
